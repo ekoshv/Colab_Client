70b713b88c1875090900bac9184e8533dc636559
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-14 12:40:19 +0200

    Update core.py

--- colab_remote/core.py
+++ colab_remote/core.py
@@ -1,22 +1,9 @@
 import requests
 import json
-from tqdm import tqdm
-import io
-import pandas as pd
-import tempfile
+import pickle
 import os
-
-
-class ProgressBytesIO(io.BytesIO):
-    def __init__(self, data, progress_bar):
-        super().__init__(data)
-        self.progress_bar = progress_bar
-
-    def read(self, n=-1):
-        chunk = super().read(n)
-        self.progress_bar.update(len(chunk))
-        return chunk
-
+from tqdm import tqdm
+from functools import partial
 
 class ColabRemote:
     def __init__(self, colab_api_url):
@@ -24,49 +11,36 @@ class ColabRemote:
         self.colab_api_url = colab_api_url
 
     def save_input_data_to_file(self, input_data):
-        temp_dir = tempfile.mkdtemp()
-        file_paths = {}
+        file_path = 'input_data.pkl'
+        with open(file_path, 'wb') as f:
+            pickle.dump(input_data, f)
+        return file_path
 
-        for key, value in input_data.items():
-            if isinstance(value, pd.DataFrame):
-                file_path = os.path.join(temp_dir, f"{key}.parquet")
-                value.to_parquet(file_path)
-                file_paths[key] = file_path
-            else:
-                file_path = os.path.join(temp_dir, f"{key}.json")
-                with open(file_path, 'w') as f:
-                    json.dump(value, f)
-                file_paths[key] = file_path
-
-        return file_paths
-
-    def send_files(self, file_paths):
-        with tqdm(desc="Uploading files", unit="file") as pbar:
-            for key, file_path in file_paths.items():
-                with open(file_path, 'rb') as f:
-                    buffer = ProgressBytesIO(f.read(), pbar)
-                    response = requests.post(f"{self.colab_api_url}/upload/{key}", data=buffer)
-                    if response.status_code != 200:
-                        raise Exception(f"Error uploading file {key}: {response.text}")
+    def progress_bar(self, pbar):
+        def update_to(total_read_len, total_to_read_len):
+            pbar.update(total_read_len)
+        return update_to
 
     def execute(self, code, input_data=None):
+        """
+        Execute code on the Colab API and return the result.
+        :param code: The code to be executed on the Colab API.
+        :param input_data: The input data for the code execution.
+        :return: The result of the code execution.
+        """
         if input_data is None:
             input_data = {}
 
-        # Save input_data to temporary files
-        file_paths = self.save_input_data_to_file(input_data)
+        file_path = self.save_input_data_to_file(input_data)
 
-        # Send the files to the server
-        self.send_files(file_paths)
-
-        # Execute the code
-        data = {'code': code}
-        try:
-            response = requests.post(f"{self.colab_api_url}/execute", json=data)
-            results = response.json()
-            return results
-        except Exception as e:
-            return {'error': str(e)}
+        with open(file_path, 'rb') as f:
+            files = {'input_data': f}
+            file_size = os.path.getsize(file_path)
+            
+            with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc="Uploading data") as pbar:
+                response = requests.post(self.colab_api_url, data={'code': code}, files=files, stream=True, hooks={'response': partial(self.progress_bar(pbar))})
+                results = response.json()
+                return results
 
     def execute_from_file(self, file_path, input_data=None):
         """
@@ -82,4 +56,3 @@ class ColabRemote:
             input_data = {}
 
         return self.execute(code, input_data)
-
