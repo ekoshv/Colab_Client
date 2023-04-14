3b90c763cb9f32eacbd7e8d7d1c32911085ff200
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-14 20:58:14 +0200

    Update core.py

--- colab_remote/core.py
+++ colab_remote/core.py
@@ -1,14 +1,10 @@
-import json
-import os
-import tempfile
-from functools import partial
-
-import pandas as pd
 import requests
-from tqdm import tqdm
+import json
+
 
 class ColabRemote:
     def __init__(self, colab_api_url):
+        """Initialize the ColabRemote class with the given Colab API URL."""
         self.colab_api_url = colab_api_url
 
     def execute(self, code, input_data=None):
@@ -21,43 +17,29 @@ class ColabRemote:
         if input_data is None:
             input_data = {}
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            # Save input_data to temporary files
-            file_paths = []
-            for key, value in input_data.items():
-                file_name = f"{key}.parquet" if isinstance(value, pd.DataFrame) else f"{key}.json"
-                file_path = os.path.join(tmp_dir, file_name)
+        data = {
+            'input_data': input_data,
+            'code': code
+        }
 
-                if isinstance(value, pd.DataFrame):
-                    value.to_parquet(file_path)
-                else:
-                    with open(file_path, "w") as f:
-                        json.dump(value, f)
+        try:
+            response = requests.post(self.colab_api_url, json=data)
+            results = response.json()
+            return results
+        except Exception as e:
+            return {'error': str(e)}
 
-                file_paths.append(file_path)
+    def execute_from_file(self, file_path, input_data=None):
+        """
+        Execute code from a file on the remote Google Colab instance.
+        :param file_path: The path to the file containing the code.
+        :param input_data: Optional input data to be used in the code execution.
+        :return: The result of the code execution.
+        """
+        with open(file_path, 'r') as code_file:
+            code = code_file.read()
 
-            # Upload the files to the Colab API
-            with tqdm(total=0, unit='B', unit_scale=True, desc="Uploading data") as pbar:
-                with open(file_paths[0], "rb") as f:
-                    files = {"input_data": (os.path.basename(file_paths[0]), f)}
-                    response = requests.post(self.colab_api_url, data={'code': code}, files=files, stream=True)
-                    response.raise_for_status()
+        if input_data is None:
+            input_data = {}
 
-                    total_size = int(response.headers.get("Content-Length", 0))
-                    if total_size:
-                        pbar.total = total_size
-
-                    chunk_size = 8192
-                    content = b""
-                    for chunk in response.iter_content(chunk_size):
-                        if chunk:
-                            pbar.update(len(chunk))
-                            content += chunk
-
-                try:
-                    results = json.loads(content.decode())
-                except json.JSONDecodeError:
-                    print(f"Unexpected response content: {content.decode()}")
-                    raise
-
-                return results
+        return self.execute(code, input_data)
