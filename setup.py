e8175d9e083707a4f699861950f168a48cc2a54c
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-14 15:04:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colab_remote',
-    version='0.3.2',
+    version='0.3.6',
     packages=find_packages(),
     install_requires=[
         'requests',
