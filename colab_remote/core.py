aafca8ae81845d0d25036306dddc1737c27b4525
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-14 20:14:40 +0200

    Update core.py

--- colab_remote/core.py
+++ colab_remote/core.py
@@ -1,14 +1,33 @@
 import requests
 import json
-import pickle
+import os
+import tempfile
 from io import BytesIO
-from functools import partial
 from tqdm import tqdm
+from functools import partial
 
 class ColabRemote:
     def __init__(self, colab_api_url):
         """Initialize the ColabRemote class with the given Colab API URL."""
         self.colab_api_url = colab_api_url
+
+    def progress_bar(self, pbar):
+        """
+        Create a progress bar callback for the requests library.
+        :param pbar: The tqdm progress bar instance.
+        :return: The custom hook function for the progress bar.
+        """
+        def hook(response, *args, **kwargs):
+            nonlocal pbar
+            total_size = int(response.headers.get("Content-Length", 0))
+            pbar.total = total_size
+            pbar.refresh()
+
+            for chunk in response.iter_content(chunk_size=8192):
+                pbar.update(len(chunk))
+                yield chunk
+
+        return hook
 
     def execute(self, code, input_data=None):
         """
@@ -20,55 +39,25 @@ class ColabRemote:
         if input_data is None:
             input_data = {}
 
-        input_data_buffer = self.save_input_data_to_file(input_data)
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # Save input_data to temporary files
+            file_paths = []
+            for key, value in input_data.items():
+                file_name = f"{key}.parquet" if isinstance(value, pd.DataFrame) else f"{key}.json"
+                file_path = os.path.join(tmp_dir, file_name)
 
-        files = {
-            'input_data': ('input_data.pickle', input_data_buffer, 'application/octet-stream')
-        }
+                if isinstance(value, pd.DataFrame):
+                    value.to_parquet(file_path)
+                else:
+                    with open(file_path, "w") as f:
+                        json.dump(value, f)
 
-        with tqdm(total=input_data_buffer.getbuffer().nbytes, unit='B', unit_scale=True, desc="Uploading data") as pbar:
-            response = requests.post(self.colab_api_url, data={'code': code}, files=files, stream=True, hooks={'response': partial(self.progress_bar(pbar))})
-            
-            if response.status_code == 200:
-                result = pickle.load(BytesIO(response.content))
-                return result
-            else:
-                return {'error': f'Request failed with status code {response.status_code}'}
+                file_paths.append(file_path)
 
-    def save_input_data_to_file(self, input_data):
-        """
-        Save input data as a pickle file.
-        :param input_data: The input data to be saved.
-        :return: A BytesIO buffer containing the input data file.
-        """
-        input_data_buffer = BytesIO()
-        pickle.dump(input_data, input_data_buffer)
-        input_data_buffer.seek(0)
-        return input_data_buffer
+            # Upload the files to the Colab API
+            files = [("input_data", (os.path.basename(file_path), open(file_path, "rb"))) for file_path in file_paths]
 
-    def progress_bar(self, pbar):
-        """
-        Create a progress bar callback for the requests library.
-        :param pbar: The tqdm progress bar instance.
-        :return: The update_to callback function for the progress bar.
-        """
-        def update_to(num_bytes, **kwargs):
-            nonlocal pbar
-            pbar.update(num_bytes)
-
-        return update_to
-
-    def execute_from_file(self, file_path, input_data=None):
-        """
-        Execute code from a file on the remote Google Colab instance.
-        :param file_path: The path to the file containing the code.
-        :param input_data: Optional input data to be used in the code execution.
-        :return: The result of the code execution.
-        """
-        with open(file_path, 'r') as code_file:
-            code = code_file.read()
-
-        if input_data is None:
-            input_data = {}
-
-        return self.execute(code, input_data)
+            with tqdm(total=0, unit='B', unit_scale=True, desc="Uploading data") as pbar:
+                response = requests.post(self.colab_api_url, data={'code': code}, files=files, stream=True)
+                results = json.loads(''.join(map(lambda x: x.decode(), self.progress_bar(pbar)(response))))
+                return results
