0f72f2118ecda1854c48be534c6c8487e2016e0d
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-16 13:32:35 +0200

    Update core.py

--- colab_remote/core.py
+++ colab_remote/core.py
@@ -1,3 +1,5 @@
+import base64
+import pickle
 import requests
 import json
 
@@ -6,15 +8,30 @@ class ColabRemote:
         """Initialize the ColabRemote class with the given Colab API URL."""
         self.colab_api_url = colab_api_url
 
-    def execute(self, code, input_data=None):
+    def serialize_data(self, data):
+        """Serialize data using pickle and encode it with base64."""
+        serialized_data = pickle.dumps(data)
+        return base64.b64encode(serialized_data).decode('utf-8')
+
+    def deserialize_data(self, data_str):
+        """Decode base64 data and deserialize it using pickle."""
+        decoded_data = base64.b64decode(data_str.encode('utf-8'))
+        return pickle.loads(decoded_data)
+
+    def execute(self, code, input_data=None, complex_input=None):
         """
         Execute code on the Colab API and return the result.
         :param code: The code to be executed on the Colab API.
         :param input_data: The input data for the code execution.
+        :param complex_input: Complex input data for the code execution.
         :return: The result of the code execution.
         """
         if input_data is None:
             input_data = {}
+
+        if complex_input is not None:
+            complex_input_str = self.serialize_data(complex_input)
+            input_data['complex_input'] = complex_input_str
 
         data = {
             'input_data': input_data,
@@ -24,15 +41,20 @@ class ColabRemote:
         try:
             response = requests.post(self.colab_api_url, json=data)
             results = response.json()
+
+            if 'complex_result' in results:
+                results['result'] = self.deserialize_data(results['complex_result'])
+
             return results
         except Exception as e:
             return {'error': str(e)}
 
-    def execute_from_file(self, file_path, input_data=None):
+    def execute_from_file(self, file_path, input_data=None, complex_input=None):
         """
         Execute code from a file on the remote Google Colab instance.
         :param file_path: The path to the file containing the code.
         :param input_data: Optional input data to be used in the code execution.
+        :param complex_input: Complex input data for the code execution.
         :return: The result of the code execution.
         """
         with open(file_path, 'r') as code_file:
@@ -41,4 +63,4 @@ class ColabRemote:
         if input_data is None:
             input_data = {}
 
-        return self.execute(code, input_data)
+        return self.execute(code, input_data, complex_input)
