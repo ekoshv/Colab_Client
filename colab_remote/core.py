91835360f0ce7fe4cb1285af3be345aee3b3d078
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-16 14:01:22 +0200

    Update core.py

--- colab_remote/core.py
+++ colab_remote/core.py
@@ -1,18 +1,14 @@
-import requests
+import base64
+import io
 import json
+import requests
+import tensorflow as tf
 
 class ColabRemote:
     def __init__(self, colab_api_url):
-        """Initialize the ColabRemote class with the given Colab API URL."""
         self.colab_api_url = colab_api_url
 
     def execute(self, code, input_data=None):
-        """
-        Execute code on the Colab API and return the result.
-        :param code: The code to be executed on the Colab API.
-        :param input_data: The input data for the code execution.
-        :return: The result of the code execution.
-        """
         if input_data is None:
             input_data = {}
 
@@ -24,17 +20,14 @@ class ColabRemote:
         try:
             response = requests.post(self.colab_api_url, json=data)
             results = response.json()
+            if 'result' in results and results['result'] is not None:
+                model_buffer = io.BytesIO(base64.b64decode(results['result'].encode('utf-8')))
+                results['result'] = tf.keras.models.load_model(model_buffer)
             return results
         except Exception as e:
             return {'error': str(e)}
 
     def execute_from_file(self, file_path, input_data=None):
-        """
-        Execute code from a file on the remote Google Colab instance.
-        :param file_path: The path to the file containing the code.
-        :param input_data: Optional input data to be used in the code execution.
-        :return: The result of the code execution.
-        """
         with open(file_path, 'r') as code_file:
             code = code_file.read()
 
